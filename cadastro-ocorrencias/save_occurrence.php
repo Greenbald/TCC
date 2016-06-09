6298ab14af57ff2408a6ce96880183a2815fe578
Paulo Mann <paulomann@id.uff.br>
2016-06-09 14:34:27 -0300

    Removing file dependencies

--- cadastro-ocorrencias/save_occurrence.php
+++ cadastro-ocorrencias/save_occurrence.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'credentials.php';
+#include 'credentials.php';
 #header('Content-Type: application/json');
 error_reporting(E_ALL);
 
@@ -11,8 +11,8 @@ $date = $_POST['date'];
 $nearby_location = $_POST['nearby_location']
 $stolen_objects = $_POST['stolen_objects']
 
-$conn = new mysqli($servername, $username, $password, $database);
-$conn->close();
+#$conn = new mysqli($servername, $username, $password, $database);
+#$conn->close();
 
 $Data = array('suburb' => $suburb, 'occurrence_type' => $occurrence_type, 'time' => $time, 'date' => $date, 'nearby_location' => $nearby_location, 'stolen_objects' => $stolen_objects);
 echo json_encode($Data);
