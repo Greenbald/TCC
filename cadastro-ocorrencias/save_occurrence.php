d44ead0706b8985c8f1ab33efbdc68a33e1ddba2
Paulo Mann <paulomann@id.uff.br>
2016-06-09 14:50:42 -0300

    Syntax error fixed

--- cadastro-ocorrencias/save_occurrence.php
+++ cadastro-ocorrencias/save_occurrence.php
@@ -1,19 +1,16 @@
 <?php
 
-#include 'credentials.php';
-#header('Content-Type: application/json');
+//header('Content-Type: application/json');
 error_reporting(E_ALL);
 
 $suburb = $_POST['suburb'];
 $occurrence_type = $_POST['occurrence_type'];
 $time = $_POST['time'];
 $date = $_POST['date'];
-$nearby_location = $_POST['nearby_location']
-$stolen_objects = $_POST['stolen_objects']
-
-#$conn = new mysqli($servername, $username, $password, $database);
-#$conn->close();
+$nearby_location = $_POST['nearby_location'];
+$stolen_objects = $_POST['stolen_objects'];
 
 $Data = array('suburb' => $suburb, 'occurrence_type' => $occurrence_type, 'time' => $time, 'date' => $date, 'nearby_location' => $nearby_location, 'stolen_objects' => $stolen_objects);
 echo json_encode($Data);
+
 ?>
