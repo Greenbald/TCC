e7f175fdd2a14c6031af1cb5d28823371ae0d639
Paulo Mann <paulomann@id.uff.br>
2016-06-08 23:06:13 -0300

    Mudando para POST

--- cadastro-ocorrencias/save_occurrence.php
+++ cadastro-ocorrencias/save_occurrence.php
@@ -4,12 +4,12 @@ include 'credentials.php';
 #header('Content-Type: application/json');
 error_reporting(E_ALL);
 
-$suburb = $_GET['suburb'];
-$occurrence_type = $_GET['occurrence_type'];
-$time = $_GET['time'];
-$date = $_GET['date'];
-$nearby_location = $_GET['nearby_location']
-$stolen_objects = $_GET['stolen_objects']
+$suburb = $_POST['suburb'];
+$occurrence_type = $_POST['occurrence_type'];
+$time = $_POST['time'];
+$date = $_POST['date'];
+$nearby_location = $_POST['nearby_location']
+$stolen_objects = $_POST['stolen_objects']
 
 $conn = new mysqli($servername, $username, $password, $database);
 $conn->close();
