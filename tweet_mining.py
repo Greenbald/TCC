8086b7eff003a6fde7c0ce9e445fb0355fa0f3d7
Paulo Roberto Mann Marques Júnior <paulomann@id.uff.br>
2016-09-19 13:31:44 -0300

    Tweet streaming algorithm working parcially

---
+++ tweet_mining.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, print_function
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+consumer_key = "6ZvBQkiTM2rX2svhAlMgg3Ekm"
+consumer_secret = "Xxyg43oq6JHoGrWS0zFXkUysscDuTk1FYIABsrbNpputlouJED"
+access_token = "2876597908-XuwL9VoPI6jw5ErlyqnlCiwGXWyzG81SDAf1gE2"
+access_token_secret = "pZc6J6mLPi5DIGUDMp5p2l1HEUDtuwZLinstQ00YMQiwS"
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+
+    """
+    def on_data(self, data):
+        #print(data)
+        #f = open('data.txt', 'w')
+        #f.write(str(data).encode('utf8'))
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(languages=['pt'], track=["e", u"é", "eu", "ele", "esse", "para", "pra", ".", "mais", "mas", u"não", "nao", "n"])
